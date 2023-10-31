11836aed403690a983dbe6328c322dd84047f800
hhuziuk <huziukwork@gmail.com>
2023-10-31 23:59:23 +0100

    updated function createTableSql

--- src/core/data-types/data-types.ts
+++ src/core/data-types/data-types.ts
@@ -6,3 +6,4 @@ export const dataType = {
     Array: { key: 'ARRAY' },
     Object: { key: 'JSON' }
 }
+
--- src/core/entities/defineEntity.ts
+++ src/core/entities/defineEntity.ts
@@ -1,8 +1,44 @@
-import {Pool} from "pg";
+const createTableSql = function(schemaName: string, schema: any) {
+    const { tableName, attributes, options } = schema;
+    const columns = Object.keys(attributes).map((attribute) => {
+        const {
+            type,
+            unique,
+            allowNull,
+            defaultValue,
+            timestamps,
+            autoIncrement,
+            primaryKey,
+        } = attributes[attribute];
+        let columnDefinition = `${attribute} ${type.key}`;
+        if (unique) {
+            columnDefinition += ' UNIQUE';
+        }
+        if (!allowNull) {
+            columnDefinition += ' NOT NULL';
+        }
+        if (defaultValue) {
+            columnDefinition += ` DEFAULT ${defaultValue}`;
+        }
+        if (primaryKey) {
+            columnDefinition += ' PRIMARY KEY';
+        }
+        if (autoIncrement) {
+            columnDefinition += ' SERIAL';
+        }
+        return columnDefinition;
+    });
 
-const pool = new Pool()
-const createTableSql = async function(schemaName: string, schema: any){
-    const {column, type, attributes, options} = schema;
+    let query = `CREATE TABLE IF NOT EXISTS ${schemaName} (${columns.join(', ')});`;
+
+    if (options) {
+        if (options.timestamps) {
+            query += `\nALTER TABLE ${schemaName} ADD COLUMN created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP;`;
+            query += `\nALTER TABLE ${schemaName} ADD COLUMN updated_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP;`;
+        }
+    }
+
+    return query;
+};
 
 
-}
