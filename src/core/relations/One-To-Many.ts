6674535f2281c29d983fce97d0673f606ac091fb
hhuziuk <huziukwork@gmail.com>
2023-11-01 16:17:38 +0100

    changed simple returning from One-To-Many.ts in createOneToManyRelation function to pool to database

--- src/core/relations/One-To-Many.ts
+++ src/core/relations/One-To-Many.ts
@@ -1,9 +1,20 @@
-export const createOneToManyRelation = function(
+import {pgConfig} from "../../configs/pgConfig";
+import {QueryResult} from "pg";
+
+export const createOneToManyRelation = async function(
     tableName: string,
     key: string,
     referenceTable: string,
     referenceKey: string
-): string {
+) {
     const query = `${key} INTEGER REFERENCES ${referenceTable}("${referenceKey}") ON DELETE SET NULL ON UPDATE CASCADE`;
-    return `ALTER TABLE ${tableName} ADD ${query};`;
+    const alterQuery = `ALTER TABLE ${tableName} ADD ${query};`;
+    try {
+        const client = await pgConfig.connect();
+        const res: QueryResult = await client.query(query);
+        client.release();
+        return res;
+    } catch (err) {
+        throw new Error(`Error doing query: ${err}`);
+    }
 };
