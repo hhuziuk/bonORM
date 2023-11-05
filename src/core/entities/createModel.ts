df9f6edd5517b8414ef80a039ec5af6760676b41
hhuziuk <huziukwork@gmail.com>
2023-11-05 09:59:17 +0100

    added implementation for find function in Model class

--- src/core/entities/createModel.ts
+++ src/core/entities/createModel.ts
@@ -1,8 +1,14 @@
-import {QueryResult} from "pg";
-import {pgConfig} from "../../configs/pgConfig";
-import {toolCommandsInterface} from "../tool-commands/tool-commands-interface";
+import { QueryResult } from "pg";
+import { pgConfig } from "../../configs/pgConfig";
+import { toolCommandsInterface } from "../tool-commands/tool-commands-interface";
 
-export class Model implements toolCommandsInterface{
+export class Model implements toolCommandsInterface {
+    private readonly tableName: string;
+
+    public constructor(tableName: string) {
+        this.tableName = tableName;
+    }
+
     private async runQuery(query: string): Promise<QueryResult> {
         try {
             const client = await pgConfig.connect();
@@ -13,55 +19,78 @@ export class Model implements toolCommandsInterface{
             throw new Error(`Error doing query: ${err}`);
         }
     }
-    async find(){
-        let query: string = `SELECT * FROM your_table_name;`;
+
+    async find(options: {
+        select?: string[];
+        relations?: string[];
+        where?: Record<string, any>;
+        order?: Record<string, 'ASC' | 'DESC'>;
+        skip?: number;
+        take?: number;
+    }): Promise<QueryResult> {
+        let query: string = `SELECT ${options.select && options.select.length > 0 
+            ? options.select.join(', ') 
+                : '*'} FROM ${this.tableName}`;
+        query += options.relations && options.relations.length > 0
+            ? `LEFT JOIN ${options.relations.join(" LEFT JOIN ")}`
+            : '';
+        query += options.where && Object.keys(options.where).length > 0
+            ? ` WHERE ${Object.keys(options.where).map(key => typeof options.where[key] === 'string' 
+                ? `${key} = '${options.where[key]}'` 
+                : `${key} = ${options.where[key]}`).join(" AND ")}`
+            : "";
+        query += options.order && Object.keys(options.order).length > 0
+            ? ` ORDER BY ${Object.keys(options.order).map(key => `${key} ${options.order[key]}`).join(", ")}`
+            : "";
+        query += options.skip && options.take
+            ? ` OFFSET ${options.skip} LIMIT ${options.take}`
+            : options.skip
+            ? ` OFFSET ${options.skip}`
+                : options.take
+                    ? ` LIMIT ${options.take}`
+                    : "";
+
         return this.runQuery(query);
     }
-    async findOne(object: Object){
-        let query: string = `SELECT * FROM your_table_name WHERE your_condition;`;
+
+    async findOne(): Promise<QueryResult> {
+        let query: string = `SELECT * FROM ${this.tableName} WHERE ;`;
         return this.runQuery(query);
     }
-    async create(object: Object){
-        let query: string = `INSERT INTO your_table_name (column1, column2, ...) VALUES (value1, value2, ...);`;
+
+    async create(): Promise<QueryResult> {
+        let query: string = `INSERT INTO ${this.tableName} `;
         return this.runQuery(query);
     }
-    async save(object: Object) {
-        let query: string = `UPDATE your_table_name SET column1 = value1, column2 = value2, ... WHERE your_condition;`;
+
+    async save(): Promise<QueryResult> {
+        let query: string = `UPDATE ${this.tableName} SET $;`;
         return this.runQuery(query);
     }
-    async deleteOne(object: Object){
-        let query: string = `DELETE FROM your_table_name WHERE your_condition;`;
+
+    async deleteOne(): Promise<QueryResult> {
+        let query: string = `DELETE FROM ${this.tableName} WHERE `;
         return this.runQuery(query);
     }
-    async deleteAll(object: Object){
-        let query: string = `DELETE FROM your_table_name;`;
+
+    async deleteAll(): Promise<QueryResult> {
+        let query: string = `DELETE FROM ${this.tableName};`;
         return this.runQuery(query);
     }
-    async createModel(schemaName: string, schema: any) {
-        const { tableName, attributes, options } = schema;
+
+    async createModel(schema: any): Promise<QueryResult> {
+        const { attributes, options } = schema;
         const columns = Object.keys(attributes).map((attribute) => {
-            const {
-                type,
-                unique,
-                allowNull,
-                defaultValue,
-                autoIncrement,
-                primaryKey,
-            } = attributes[attribute];
+            const { type, unique, allowNull, defaultValue, autoIncrement, primaryKey } = attributes[attribute];
             let columnDefinition: string = `${attribute} ${type.key}`;
-            (unique) ? columnDefinition += ' UNIQUE' : '';
-            (!allowNull) ? columnDefinition += ' NOT NULL' : '';
-            (defaultValue) ? columnDefinition += ` DEFAULT ${defaultValue}` : '';
-            (primaryKey) ? columnDefinition += ' PRIMARY KEY' : '';
-            (autoIncrement) ? columnDefinition += ' SERIAL' : '';
+            unique ? (columnDefinition += " UNIQUE") : "";
+            !allowNull ? (columnDefinition += " NOT NULL") : "";
+            defaultValue ? (columnDefinition += ` DEFAULT ${defaultValue}`) : "";
+            primaryKey ? (columnDefinition += " PRIMARY KEY") : "";
+            autoIncrement ? (columnDefinition += " SERIAL") : "";
             return columnDefinition;
         });
-        let query: string = `CREATE TABLE IF NOT EXISTS ${schemaName} (${columns.join(', ')});`;
+        let query: string = `CREATE TABLE IF NOT EXISTS ${this.tableName} (${columns.join(", ")});`;
         return this.runQuery(query);
     }
-
-
 }
-
-
-
--- src/core/tool-commands/tool-commands-interface.ts
+++ src/core/tool-commands/tool-commands-interface.ts
@@ -1,7 +1,14 @@
 import {QueryResult} from "pg";
 
 export interface toolCommandsInterface {
-    find(): Promise<QueryResult<any>>;
+    find(options: {
+        select?: string[];
+        relations?: string[];
+        where?: Record<string, any>;
+        order?: Record<string, 'ASC' | 'DESC'>;
+        skip?: number;
+        take?: number;
+    }): Promise<QueryResult>
     findOne(object: Object): Promise<any>;
     create(object: Object): Promise<any>;
     save(object: Object): Promise<QueryResult<any>>;
