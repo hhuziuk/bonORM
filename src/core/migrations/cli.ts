d528336973317dbda0d51371cbf8a732a0edf71e
hhuziuk <huziukwork@gmail.com>
2023-11-13 15:09:32 +0100

    added path to migration and changed command for it's generation

--- src/core/migrations/cli.ts
+++ src/core/migrations/cli.ts
@@ -2,7 +2,7 @@ import yargs, { Arguments } from 'yargs';
 import { generateMigration, runMigration, rollbackMigration } from './createMigration';
 
 yargs
-    .command('generate:migration', 'Generate a new migration', {}, generateMigration)
-    .command('up:migration', 'Apply a migration', {}, (argv: Arguments) => runMigration(argv.name as string))
-    .command('down:migration', 'Rollback a migration', {}, (argv: Arguments) => rollbackMigration(argv.name as string))
+    .command('generate:migration <path>', 'Generate a new migration', {}, (argv: Arguments) => generateMigration(argv.path))
+    .command('up:migration <name>', 'Apply a migration', {}, (argv: Arguments) => runMigration(argv.name as string))
+    .command('down:migration <name>', 'Rollback a migration', {}, (argv: Arguments) => rollbackMigration(argv.name as string))
     .argv;
--- src/core/migrations/createMigration.ts
+++ src/core/migrations/createMigration.ts
@@ -1,30 +1,29 @@
 import { Migration } from "./migration-test";
-import { QueryResult } from 'pg';
 import fs from 'fs';
-import {queryResult} from "pg-promise";
 
 export const generateMigration = (argv: any) => {
-    const createFileName = `MigrationV${new Date().getTime()}.ts`;
+    const path = argv.path || '.';
+    const createFileName = `${path}/MigrationV${new Date().getTime()}.ts`;
     const migrationFile = `
-        import {MigrationInterface} from "./migrationInterface";
-        import {QueryResult} from "pg";
-        export class ${createFileName.replace('.ts', '')} implements MigrationInterface{
-            migrationName = '${createFileName.replace('.ts', '')}';
-            public async up(query: QueryResult){
-                
-            }
-        
-            public async down(query: QueryResult){
-        
-            }
-        }
-`;
+    import {MigrationInterface} from "./migrationInterface";
+    import {QueryResult} from "pg";
+    export class ${createFileName.replace('.ts', '')} implements MigrationInterface {
+      migrationName = '${createFileName.replace('.ts', '')}';
+      public async up(query: QueryResult) {
+        // Your migration logic
+      }
+
+      public async down(query: QueryResult) {
+        // Your rollback logic
+      }
+    }
+  `;
     fs.writeFileSync(createFileName, migrationFile);
     console.log(`Migration ${createFileName} generated successfully.`);
 };
 
 export const runMigration = async (name: string) => {
-    const migrationModule = require(`./${name}`)
+    const migrationModule = require(`./${name}`);
     const migration = new Migration[name]();
     await migration.up();
 };
