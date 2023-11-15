90c84be37633e1fe9281bcd0172aaffb3d76b341
hhuziuk <huziukwork@gmail.com>
2023-11-15 11:18:14 +0100

    changed datatype object to enum

--- src/core/data-types/data-types.ts
+++ src/core/data-types/data-types.ts
@@ -1,9 +1,9 @@
-export const dataType = {
-    Integer: { key: 'INTEGER' },
-    String: { key: 'VARCHAR' },
-    Date: { key: 'DATE' },
-    Boolean: { key: 'BOOLEAN' },
-    Array: { key: 'ARRAY' },
-    Object: { key: 'JSON' },
-    Enum: {key: 'ENUM'}
+export enum dataType {
+    Integer = 'INTEGER',
+    String = 'VARCHAR',
+    Date = 'DATE',
+    Boolean = 'BOOLEAN',
+    Array = 'ARRAY',
+    Object = 'JSON',
+    Enum = 'ENUM'
 };
