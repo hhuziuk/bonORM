779cffe9b14f287832eca094e8dc30d6dddc4c6c
hhuziuk <huziukwork@gmail.com>
2023-11-17 19:47:12 +0100

    updated .gitignore

--- src/configs/pgConfig.ts
+++ src/configs/pgConfig.ts
@@ -1,9 +1,9 @@
 import { Pool } from 'pg';
 
 export const pgConfig = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'postgres',
-    password: 'root',
+    user: 'your data',
+    host: 'your data',
+    database: 'your data',
+    password: 'your data',
     port: 5432,
 });
