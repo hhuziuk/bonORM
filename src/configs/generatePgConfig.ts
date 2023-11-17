bf1c6d3dfd2ddbae4535add8e90807d37368753b
hhuziuk <huziukwork@gmail.com>
2023-11-17 22:32:55 +0100

    created config generator for PostgreSQL

---
+++ src/configs/generatePgConfig.ts
@@ -0,0 +1,18 @@
+import fs from "fs";
+
+export const generatePgConfig = (argv: any) => {
+    const path = argv.path || '.';
+    const createFileName = `pgConfig.ts`;
+    const configFile = `
+        import { Pool } from 'pg';
+        export const pgConfig = new Pool({
+            user: 'your data',
+            host: 'your data',
+            database: 'your data',
+            password: 'your data',
+            port: 5432,
+        });
+  `;
+    fs.writeFileSync(createFileName, configFile);
+    console.log(`Configuration ${createFileName} generated successfully.`);
+};
