34d8997f2a8c6e20250418c07899ca015aa44296
hhuziuk <huziukwork@gmail.com>
2023-11-18 17:16:55 +0100

    beautified code for pgConfig generation

--- src/configs/generateMySqlConfig.ts
+++ src/configs/generateMySqlConfig.ts
@@ -4,15 +4,14 @@ export const generateMySqlConfig = (argv: any) => {
     const path = argv.path || '.';
     const createFileName = `${path}/mySqlConfig.ts`;
     const configFile = `
-        import {createPool} from 'mysql';
-
-        export const mySqlConfig  = createPool({
-            // connectionLimit : 10,
-            host: 'your data',
-            user: 'your data',
-            password: 'your data',
-            database: 'your data'
-        });
+import {createPool} from 'mysql';
+export const mySqlConfig  = createPool({
+    // connectionLimit : 10,
+    host: 'your data',
+    user: 'your data',
+    password: 'your data',
+    database: 'your data'
+});
   `;
     fs.writeFileSync(createFileName, configFile);
     console.log(`Configuration ${createFileName} generated successfully.`);
