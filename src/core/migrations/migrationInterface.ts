95d3eaee6d32f6c72c3a8741253660b903476437
hhuziuk <huziukwork@gmail.com>
2023-11-24 15:35:01 +0100

    1.0.6

--- src/core/migrations/migrationInterface.ts
+++
@@ -1,7 +0,0 @@
-import {QueryResult} from "pg";
-
-export interface MigrationInterface {
-    migrationName: string;
-    up(query?: QueryResult): Promise<void>;
-    down(query?: QueryResult): Promise<void>;
-}
