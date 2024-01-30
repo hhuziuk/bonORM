13d58ec715e3cc7f30de66f36118f25734da5a5d
hhuziuk <huziukwork@gmail.com>
2024-01-30 21:24:47 +0100

    updated data-types.ts: added some new datatypes, deleted Array datatype and wrote some new description comments

--- src/core/data-types/data-types.ts
+++ src/core/data-types/data-types.ts
@@ -1,10 +1,13 @@
 export enum dataType {
-    Integer = 'INTEGER',
-    String = 'VARCHAR',
-    Date = 'DATE',
-    Boolean = 'BOOLEAN',
-    Array = 'ARRAY',
-    Object = 'JSON',
-    Enum = 'ENUM',
-    UUID = 'UUID'
+    Integer = 'INTEGER', // signed four-byte integer
+    String = 'VARCHAR', // variable-length character string
+    Date = 'DATE', // calendar date (year, month, day)
+    Boolean = 'BOOLEAN', // logical Boolean (true/false)
+    Object = 'JSON', // textual JSON data
+    ObjectB = 'JSONB', // binary JSON data, decomposed
+    Enum = 'ENUM', // enumeration
+    UUID = 'UUID', // universally unique identifier
+    XML = 'XML', // XML data
+    Money = 'MONEY', // currency amount
+    Inet = 'INET', // IPv4 or IPv6 host address
 };
