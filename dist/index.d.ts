414a2fbbf4feac09d179acbcb3c48b6f70a7ebf0
hhuziuk <huziukwork@gmail.com>
2024-01-31 23:47:22 +0100

    resolved problem with configuration file's name in imports

--- dist/index.d.ts
+++
@@ -1,9 +0,0 @@
-export * from './src/core/data-types/PgDataTypes';
-export * from './src/validators/validator';
-export * from './src/core/entities/createModel';
-export * from './src/core/migrations/createMigration';
-export * from './src/core/migrations/migrationInterface';
-export * from './src/core/relations/One-To-One';
-export * from './src/core/relations/Many-To-Many';
-export * from './src/core/relations/One-To-Many';
-//# sourceMappingURL=index.d.ts.map
--- dist/src/configs/generateMySqlConfig.d.ts
+++
@@ -1,2 +0,0 @@
-export declare const generateMySqlConfig: (argv: any) => void;
-//# sourceMappingURL=generateMySqlConfig.d.ts.map
