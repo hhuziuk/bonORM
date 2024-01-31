d60259078477f6990b7eba7a35b645bf91ea2c27
hhuziuk <huziukwork@gmail.com>
2024-01-31 23:47:44 +0100

    resolved problem with configuration file's name in imports

---
+++ dist/src/core/migrations/createMigration.d.ts
@@ -0,0 +1,4 @@
+export declare const generateMigration: (argv: any) => void;
+export declare const runMigration: (argv: any) => Promise<void>;
+export declare const rollbackMigration: (argv: any) => Promise<void>;
+//# sourceMappingURL=createMigration.d.ts.map
