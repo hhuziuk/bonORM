30c34d4546bc5ff1800d0178a266c00dc26af159
hhuziuk <huziukwork@gmail.com>
2024-02-04 00:16:59 +0100

    removed enum datatype in core/data-types/PgDataTypes.ts

--- src/core/data-types/PgDataTypes.ts
+++ src/core/data-types/PgDataTypes.ts
@@ -7,7 +7,6 @@ export enum dataType {
     Boolean = 'BOOLEAN', // logical Boolean (true/false)
     Object = 'JSON', // textual JSON data
     ObjectB = 'JSONB', // binary JSON data, decomposed
-    Enum = 'ENUM', // enumeration
     UUID = 'UUID', // universally unique identifier
     XML = 'XML', // XML data
     Money = 'MONEY', // currency amount
