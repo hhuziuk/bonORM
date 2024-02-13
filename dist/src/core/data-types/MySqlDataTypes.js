ada61a088265ae4f493c00c2adff08731d0ae505
hhuziuk <huziukwork@gmail.com>
2024-02-13 22:26:59 +0100

    updated Data Types description in README.md and expanded enum with types in mySqlDataTypes and PgDataTypes

--- dist/src/core/data-types/MySqlDataTypes.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.mySqlDataType = void 0;
-var mySqlDataType;
-(function (mySqlDataType) {
-})(mySqlDataType = exports.mySqlDataType || (exports.mySqlDataType = {}));
-;
