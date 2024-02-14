9a1ca10a36d3e7c802d3ba668688fafffbb566cf
hhuziuk <huziukwork@gmail.com>
2024-02-14 21:01:17 +0100

    resolved some little issues and added realisation for uuid indexes in PrimaryGeneratedColumn() in decorators.ts

--- dist/src/configs/generatePgConfig.d.ts
+++
@@ -1,2 +0,0 @@
-export declare const generatePgConfig: (argv: any) => void;
-//# sourceMappingURL=generatePgConfig.d.ts.map
--- dist/src/core/options/ColumnGeneralOptions.d.ts
+++
@@ -1,9 +0,0 @@
-export interface ColumnGeneralOptions {
-    unique?: boolean;
-    defaultValue?: any;
-    allowNull?: boolean;
-    onUpdate?: string;
-    generated?: boolean | "increment" | "uuid" | "rowid" | "identity";
-    name?: string;
-}
-//# sourceMappingURL=ColumnGeneralOptions.d.ts.map
