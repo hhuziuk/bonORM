7308b78ed6ee4fbd56ad460f1de77c916a7b6a68
hhuziuk <huziukwork@gmail.com>
2024-02-17 17:53:05 +0100

    added atribute , deleted some unuseful atttributes from ColumnGeneralOption.ts and rewrited ternary operators on simple if-else statements

--- dist/src/core/options/ColumnGeneralOptions.d.ts
+++ dist/src/core/options/ColumnGeneralOptions.d.ts
@@ -1,11 +1,9 @@
 export interface ColumnGeneralOptions {
-    primaryKey: boolean;
-    autoIncrement: boolean;
-    length: number;
-    width: number;
-    update: boolean;
-    insert: boolean;
-    name: string;
+    primaryKey?: boolean;
+    autoIncrement?: boolean;
+    length?: number;
+    width?: number;
+    name?: string;
     type: string;
     unique?: boolean;
     defaultValue?: string;
