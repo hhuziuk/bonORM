5e957819aae10c649986dd0392b52b5b0102e46c
hhuziuk <huziukwork@gmail.com>
2024-02-17 17:53:05 +0100

    added attribute 'unique', deleted some un useful attributes from ColumnGeneralOption.ts and rewrote ternary operators on simple if-else statements

--- dist/src/core/queries/buildTableQuery.js
+++ dist/src/core/queries/buildTableQuery.js
@@ -3,9 +3,43 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const uuid_1 = require("uuid");
 class buildTableQuery {
     static buildColumnDefinition({ propertyKey, options }) {
-        const { type, allowNull, defaultValue, autoIncrement, primaryKey } = options;
+        const { type, allowNull, defaultValue, autoIncrement, primaryKey, width, onUpdate, name, length, unique } = options;
         let columnDefinition = `${propertyKey} ${type}`;
-        columnDefinition += (!allowNull) ? " NOT NULL" : "";
+        // allowNull
+        if (!allowNull) {
+            columnDefinition += " NOT NULL";
+        }
+        else {
+            columnDefinition += "";
+        }
+        // name for column
+        if (name) {
+            columnDefinition += ` ${name}`;
+        }
+        // unique
+        if (unique) {
+            columnDefinition += " UNIQUE";
+        }
+        // length
+        if (length) {
+            columnDefinition += `(${length})`;
+        }
+        // width (for MySQL)
+        if (width && process.env.DB_TYPE === 'mysql') {
+            columnDefinition += ` WIDTH ${width}`;
+        }
+        // onUpdate (for MySQL)
+        if (onUpdate && process.env.DB_TYPE === 'mysql') {
+            columnDefinition += ` ON UPDATE ${onUpdate}`;
+        }
+        // primaryKey
+        if (primaryKey) {
+            columnDefinition += " PRIMARY KEY";
+        }
+        else {
+            columnDefinition += "";
+        }
+        // defaultValue
         if (defaultValue) {
             columnDefinition += ` DEFAULT '${defaultValue}'`;
         }
@@ -14,12 +48,16 @@ class buildTableQuery {
                 columnDefinition += ` DEFAULT ${type === 'TIMESTAMP' ? 'CURRENT_TIMESTAMP' : 'CURRENT_DATE'}`;
             }
         }
-        columnDefinition += (primaryKey) ? " PRIMARY KEY" : "";
         if (type === 'UUID') {
             columnDefinition += (process.env.DB_TYPE === 'postgres') ? " DEFAULT uuid_generate_v4()" : ""; // PostgreSQL
             columnDefinition += (process.env.DB_TYPE === 'mysql') ? ` DEFAULT '${(0, uuid_1.v4)()}'` : ""; // MySQL
         }
-        columnDefinition += (autoIncrement && type === 'INTEGER') ? " GENERATED ALWAYS AS IDENTITY" : "";
+        if (autoIncrement && type === 'INTEGER') {
+            columnDefinition += " GENERATED ALWAYS AS IDENTITY";
+        }
+        else {
+            columnDefinition += "";
+        }
         return columnDefinition;
     }
     static buildCreateTableQuery(tableName, columnsStrings) {
--- src/core/queries/buildTableQuery.ts
+++ src/core/queries/buildTableQuery.ts
@@ -1,11 +1,50 @@
-import {ColumnGeneralOptions} from "../options/ColumnGeneralOptions";
 import { v4 } from 'uuid';
 
 class buildTableQuery {
     static buildColumnDefinition({ propertyKey, options }: any): string {
-        const { type, allowNull, defaultValue, autoIncrement, primaryKey } = options;
+        const { type, allowNull, defaultValue, autoIncrement, primaryKey, width, onUpdate, name, length, unique } = options;
         let columnDefinition: string = `${propertyKey} ${type}`;
-        columnDefinition += (!allowNull) ? " NOT NULL" : "";
+
+        // allowNull
+        if (!allowNull) {
+            columnDefinition += " NOT NULL";
+        } else {
+            columnDefinition += "";
+        }
+
+        // name for column
+        if (name) {
+            columnDefinition += ` ${name}`;
+        }
+
+        // unique
+        if (unique) {
+            columnDefinition += " UNIQUE";
+        }
+
+        // length
+        if (length) {
+            columnDefinition += `(${length})`;
+        }
+
+        // width (for MySQL)
+        if (width && process.env.DB_TYPE === 'mysql') {
+            columnDefinition += ` WIDTH ${width}`;
+        }
+
+        // onUpdate (for MySQL)
+        if (onUpdate && process.env.DB_TYPE === 'mysql') {
+            columnDefinition += ` ON UPDATE ${onUpdate}`;
+        }
+
+        // primaryKey
+        if (primaryKey) {
+            columnDefinition += " PRIMARY KEY";
+        } else {
+            columnDefinition += "";
+        }
+
+        // defaultValue
         if (defaultValue) {
             columnDefinition += ` DEFAULT '${defaultValue}'`;
         } else {
@@ -13,19 +52,25 @@ class buildTableQuery {
                 columnDefinition += ` DEFAULT ${type === 'TIMESTAMP' ? 'CURRENT_TIMESTAMP' : 'CURRENT_DATE'}`;
             }
         }
-        columnDefinition += (primaryKey) ? " PRIMARY KEY" : "";
+
         if (type === 'UUID') {
             columnDefinition += (process.env.DB_TYPE === 'postgres') ? " DEFAULT uuid_generate_v4()" : ""; // PostgreSQL
             columnDefinition += (process.env.DB_TYPE === 'mysql') ? ` DEFAULT '${v4()}'` : ""; // MySQL
         }
-        columnDefinition += (autoIncrement && type === 'INTEGER') ? " GENERATED ALWAYS AS IDENTITY" : "";
+
+        if (autoIncrement && type === 'INTEGER') {
+            columnDefinition += " GENERATED ALWAYS AS IDENTITY";
+        } else {
+            columnDefinition += "";
+        }
+
         return columnDefinition;
     }
+
     static buildCreateTableQuery(tableName: string, columnsStrings: string[]): string {
         const columnsQuery = columnsStrings.join(", ");
         return `CREATE TABLE IF NOT EXISTS ${tableName} (${columnsQuery});`;
     }
-
 }
 
 export default buildTableQuery;
