8a002949f116c99fc65ae5bb6717e91e5f25f7b7
hhuziuk <huziukwork@gmail.com>
2024-02-19 20:47:12 +0100

    added export in MySQL config generator

--- src/configs/generateMySqlConfig.ts
+++ src/configs/generateMySqlConfig.ts
@@ -14,6 +14,7 @@ const mySqlConfig = () => {
         database: 'your data'
     });
 };
+export default mySqlConfig;
   `;
     fs.writeFileSync(createFileName, configFile);
     console.log(`Configuration ${createFileName} generated successfully.`);
